f978d44b0709b573a40c8a1ae4fe781f18e0957d
SteveNascimento <steve.melo.nascimento@gmail.com>
2023-04-24 18:09:31 -0300

    Adicionando delete

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import multer from "multer";
 import path from "path";
 import { PrismaClient } from "@prisma/client";
+import fs from "fs";
 
 const app = expresss();
 const port = 5000;
@@ -24,7 +25,7 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix + ".pdf");
+    cb(null, file.originalname + "-" + uniqueSuffix + ".pdf");
   },
 });
 
@@ -43,24 +44,30 @@ const upload = multer({
 });
 
 // rota de upload
-app.post("/upload", upload.single("pdf"), async (req, res) => {
+app.post("/upload", upload.array("file"), async (req, res) => {
   // Aqui vou salvar o arquivo no banco de dados
   // e retornar o id do arquivo salvo
   try {
-    if (!req.file) {
-      return res.status(400).json({ error: "Arquivo não enviado" });
-    }
+    // Verifica se veio com arquivo
+    // Se não veio, retorna erro
 
-    const { originalname, path } = req.file;
+    // Se vier com arquivo, salva no banco
+    const files = req.files as Express.Multer.File[];
 
-    const file = await prisma.file.create({
-      data: {
-        name: originalname,
-        path: path,
-      },
+    const promises = files.map(async (file) => {
+      const fileSaved = await prisma.file.create({
+        data: {
+          name: file.filename,
+          path: file.path,
+        },
+      });
+
+      return fileSaved;
     });
 
-    res.send(file).status(200);
+    const filesSaved = await Promise.all(promises);
+
+    return res.json(filesSaved);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Erro ao fazer upload do arquivo" });
@@ -102,6 +109,39 @@ app.get("/files", async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Erro ao listar os arquivos" });
+  }
+});
+
+// rota de exclusão
+app.delete("/files/:id", async (req, res) => {
+  // Aqui vou buscar o arquivo no banco de dados
+  // e excluir o arquivo do banco e do disco
+  try {
+    const { id } = req.params;
+
+    const file = await prisma.file.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!file) {
+      return res.status(404).json({ error: "Arquivo não encontrado" });
+    }
+
+    await prisma.file.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    // Exclui o arquivo do disco
+    fs.unlinkSync(file.path);
+
+    return res.json({ message: "Arquivo excluído com sucesso" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erro ao excluir o arquivo" });
   }
 });
 
